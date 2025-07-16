8366bb44d6b265b09c0b7d2497949c5252a29f9c
StEsref <86866446+zamborna@users.noreply.github.com>
2025-07-16 13:03:33 +0300

    Update useStrore.ts

--- useStrore.ts
+++ useStrore.ts
@@ -1,1 +1,18 @@
+type State = {
+  rooms: Room[];
+  generateLayout: (input: LayoutInput) => void;
+  updateRoom: (id: string, updates: Partial<Room>) => void;
+};
 
+export const useStore = create<State>((set) => ({
+  rooms: [],
+  generateLayout: ({ roomCount, minSize, maxSize, type }) => {
+    // (same as before)
+  },
+  updateRoom: (id, updates) =>
+    set((state) => ({
+      rooms: state.rooms.map((room) =>
+        room.id === id ? { ...room, ...updates } : room
+      ),
+    })),
+}));
