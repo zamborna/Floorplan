04af14474aa63aa4b8fb6a9c6801a339baf1a7e1
StEsref <86866446+zamborna@users.noreply.github.com>
2025-07-16 13:04:47 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,14 @@
+import InputForm from './components/InputForm';
+import FloorPlanCanvas from './components/FloorPlanCanvas';
+import ExportControls from './components/ExportControls';
 
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>Floor Plan Generator</h1>
+      <InputForm />
+      <FloorPlanCanvas />
+      <ExportControls />
+    </div>
+  );
+}
