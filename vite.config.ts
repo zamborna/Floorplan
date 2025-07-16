b1e795be91c6f4c92189d9bba2abeba6ca20fe87
StEsref <86866446+zamborna@users.noreply.github.com>
2025-07-16 13:32:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/floorplan-app/', // change to your repo name
-  plugins: [react()],
+  base: './',
+  plugins: [react()]
 });
